18a0093f1803aabae56d533f371e0063ebb43b97
xfmo <courade@hotmail.com>
2013-07-17 14:35:06 +0800

    update decorate

--- decorate_3.py
+++ decorate_3.py
@@ -4,6 +4,10 @@
 def deco(func):
     def _deco(*args,**kwargs):
         print("before %s called " % func.__name__)
+        if 'd' in kwargs.keys():
+            kwargs['d']+=" append"
+
+        print '参数',args,kwargs
         ret=func(*args,**kwargs)
         print("after %s called " % func.__name__)
         print ret
@@ -14,9 +18,10 @@ def myfunc(a,b):
     print("myfunc(%s,%s) called " %(a,b))
     return a+b
 @deco
-def myfunc2(a,b,c):
+def myfunc2(a,b,c,**kwargs):
     print("myfunc(%s,%s,%s) called " %(a,b,c))
+    print 'function2', kwargs
     return a+b+c
 
 print myfunc(1,3)
-myfunc2(1,3,4)
+myfunc2(1,3,4,d="this is test")
